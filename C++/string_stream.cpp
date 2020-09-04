17440f6d46a7dff4b34fac5d5261ae5d2876d96b
varun-2001 <57247403+varun-2001@users.noreply.github.com>
2020-09-05 00:57:49 +0530

    String Stream in C++

---
+++ C++/string_stream.cpp
@@ -0,0 +1,31 @@
+// https://www.hackerrank.com/challenges/c-tutorial-stringstream/problem
+
+#include <sstream>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+vector<int> parseInts(string str) {
+    stringstream sti(str);
+    vector<int> output;
+    int temp;
+    char ch;
+    while (sti>>temp){
+        output.push_back(temp);
+        sti>>ch;
+    }
+    return output;
+
+
+}
+
+int main() {
+    string str;
+    cin >> str;
+    vector<int> integers = parseInts(str);
+    for(int i = 0; i < integers.size(); i++) {
+        cout << integers[i] << "\n";
+    }
+    
+    return 0;
+}
