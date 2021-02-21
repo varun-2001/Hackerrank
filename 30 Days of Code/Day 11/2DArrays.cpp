834671a58ec35855867ac5edddd1c58357fd1663
varun-2001 <57247403+varun-2001@users.noreply.github.com>
2021-02-21 12:41:19 +0530

    Day 11

---
+++ 30 Days of Code/Day 11/2DArrays.cpp
@@ -0,0 +1,34 @@
+// https://www.hackerrank.com/challenges/30-2d-arrays/problem
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main()
+{
+    vector<vector<int>> arr(6);
+    for (int i = 0; i < 6; i++) {
+        arr[i].resize(6);
+
+        for (int j = 0; j < 6; j++) {
+            cin >> arr[i][j];
+        }
+
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+    // MANUALLY CHOOSE ELEMENTS
+    int max=INT_MIN;
+    int sum=0;
+    for (int i=0;i<4;i++){
+        for (int j=0;j<4;j++){
+            sum=arr[i][j]+arr[i][j+1]+arr[i][j+2]
+                        +arr[i+1][j+1]
+                +arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2];
+            if (sum>max) max=sum;
+        }
+    }
+    cout<<max;
+    return 0;
+}
